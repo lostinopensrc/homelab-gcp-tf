5e0599fb2cf65017771cc2bfafe52d5beecbad06
lostinopensrc <lostinopensrc@gmail.com>
2023-03-20 12:41:11 +0530

    corrected output file

--- outputs.tf
+++ outputs.tf
@@ -15,26 +15,21 @@
  */
 
 output "network" {
-  value       = google_compute_network.network
+  value       = module.vpc.network
   description = "The VPC resource being created"
 }
 
 output "network_name" {
-  value       = google_compute_network.network.name
+  value       = module.vpc.network.name
   description = "The name of the VPC being created"
 }
 
 output "network_id" {
-  value       = google_compute_network.network.id
+  value       = module.vpc.network.id
   description = "The ID of the VPC being created"
 }
 
-output "network_self_link" {
-  value       = google_compute_network.network.self_link
-  description = "The URI of the VPC being created"
-}
-
 output "project_id" {
-  value       = var.shared_vpc_host && length(google_compute_shared_vpc_host_project.shared_vpc_host) > 0 ? google_compute_shared_vpc_host_project.shared_vpc_host.*.project[0] : google_compute_network.network.project
+  value       = module.vpc.project_id
   description = "VPC project id"
 }

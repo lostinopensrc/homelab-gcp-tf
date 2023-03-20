dc78d9c03a95ab1c29f1cca1327d9b3d8639a637
lostinopensrc <lostinopensrc@gmail.com>
2023-03-20 12:56:08 +0530

    reduced attributes from root main.tf

--- main.tf
+++ main.tf
@@ -20,10 +20,5 @@
 module "vpc" {
   source                          = "./modules/vpc"
   name                            = var.network_name
-  auto_create_subnetworks         = var.auto_create_subnetworks
-  routing_mode                    = var.routing_mode
   project                         = var.project_id
-  description                     = var.description
-  delete_default_routes_on_create = var.delete_default_internet_gateway_routes
-  mtu                             = var.mtu
 }

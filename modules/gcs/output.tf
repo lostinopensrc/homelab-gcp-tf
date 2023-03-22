1aae35c29370a82d6bfa405f4e6cb378f717d38b
lostinopensrc <lostinopensrc@gmail.com>
2023-03-22 20:00:00 +0530

    added output for gcs module

---
+++ modules/gcs/output.tf
@@ -0,0 +1,10 @@
+output "bucket" {
+  description = "The created storage bucket"
+  value       = google_storage_bucket.bucket
+}
+
+output "name" {
+  description = "Bucket name."
+  value       = google_storage_bucket.bucket.name
+}
+
